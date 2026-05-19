69dc0d9d0559752f18b6198f3ee85b720b87738f
gmstghost <lukas0409@naver.com>
2026-05-20 00:30:24 +0900

    ✨ server.js 수정 (불필요한 주석 수정)

--- html/scripts/server.js
+++ html/scripts/server.js
@@ -41,9 +41,6 @@ async function scrapeScholarships() {
                 let displayDday = countText;
                 if (countText.includes("D-0") || stateText.includes("오늘")) displayDday = "D-DAY";
 
-                // href 속성값을 안전하게 추출 (예: /scholarship/view.html?id=123)
-                // const relativeLink = titleEl.getAttribute('href') || "";
-
                 return {
                     title: titleText,
                     org: orgText,
@@ -52,20 +49,13 @@ async function scrapeScholarships() {
                     stateClass: category, 
                     dday: displayDday,
                     link: titleEl.href
-                    // 도메인을 앞에 붙여서 절대 경로로 완성해줍니다.
-                    // ink: relativeLink.startsWith('http') ? relativeLink : `https://www.dreamspon.com${relativeLink}`
                 };
             }).filter(item => item !== null);
         });
         cachedData = data;
         lastUpdated = new Date().toLocaleString('ko-KR');
         console.log(`[${lastUpdated}] 데이터 크롤링 성공!`);
-    // } catch (e) { 
-    //     console.error("크롤링 중 에러 발생:", e); 
-    // } finally { 
-    //     if (browser) await browser.close(); 
-    // }
-            console.log(`[${lastUpdated}] 데이터 크롤링 성공!`);
+
     } catch (e) { 
         console.error("크롤링 중 에러 발생:", e); 
     } finally { 
