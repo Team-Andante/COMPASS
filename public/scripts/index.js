c0c52172aca5b4b660706fc0a64150f3e2aa55d5
dltmdals <lsmin062300@gmail.com>
2026-05-31 14:38:44 +0900

    html마다 navbar에 서비스 소개 버튼 href 다시 걸어야됨

--- public/scripts/index.js
+++
@@ -1,9 +0,0 @@
-import { nextPage } from "./toast.js";
-
-const scholarship = document.querySelector('a');
-
-if (scholarship) {
-    scholarship.addEventListener('click', (e) => {
-        console.log("buttom");
-    });
-}
